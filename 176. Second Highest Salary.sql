15534fe2806ade542e7463dddc4dc53f0c68f082
Samir Bharati <104520499+samirbharati@users.noreply.github.com>
2025-10-08 09:58:18 +0530

    176. Second Highest Salary.sql

--- 176. Second Highest Salary.sql
+++ 176. Second Highest Salary.sql
@@ -1,5 +1,5 @@
 select (select distinct salary from Employee order by salary desc limit 1 offset 1) as SecondHighestSalary;
 
-OR
+--OR
 
 select max(salary) as SecondHighestSalary from employee where salary < (select max(salary) from employee);
