5e00c25034b4f0d82d65e059d4fa2cf8abd709f1
Samir Bharati <104520499+samirbharati@users.noreply.github.com>
2026-03-04 22:22:33 +0530

    Code Testcase Testcase Test Result 242. Valid Anagram.java

---
+++ Code Testcase Testcase Test Result 242. Valid Anagram.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()) return false;
+        else{
+            char[] c1 = s.toCharArray();
+            char[] c2 = t.toCharArray();
+
+            Arrays.sort(c1);
+            Arrays.sort(c2);
+
+            if(Arrays.equals(c1, c2)) return true;
+            else return false;
+        }
+    }
+}
